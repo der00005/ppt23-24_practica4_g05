d678e48d06cb6f4494b761878e2a615f208ef220
der00005 <der00005@red.ujaen.es>
2023-12-30 20:37:19 +0100

    Final

--- ppt23-24_practica4_g05/practica4/src/main/java/ppt2324/practica4/HttpConnection.java
+++ ppt23-24_practica4_g05/practica4/src/main/java/ppt2324/practica4/HttpConnection.java
@@ -49,7 +49,7 @@ public HttpConnection(Socket s) {
 
                     String line = bis.readLine();//linea de peticion -> GET Ruta HTTP1.1
                     String partes[] = line.split(" ");
-                    //partes[1]="192.168.1.10";
+                    
                     if (partes.length == 3) {
                           System.out.println("Cabecera host:[" +partes[1]+"]: ");
                         if (partes[0].compareToIgnoreCase("get") == 0) {
@@ -59,16 +59,15 @@ public HttpConnection(Socket s) {
                                 //dos.write(("ECO " + line + "\r\n").getBytes());
                                 //dos.flush();
                             }
-
-                            //abrir archivo
+                            //Abrir archivo
                             byte[] data = readFile(partes[1]);
                             
                             if(data == null){
                                 
                             }else{
                             dos.write("HTTP/1.1 200 OK\r\n".getBytes());
-                            dos.write(("Method: "+partes[0]+"\r\n").getBytes());
-                            dos.write(("Content_Type:"+getContentType(partes[1])+"\r\n").getBytes());
+                            dos.write(("Method: "+partes[0]+"\r\n").getBytes()); // En la práctica pedía el método
+                            dos.write(("Content_Type:"+getContentType(partes[1])+"\r\n").getBytes()); // Qué tipo es lo añadido detrás de la IP (index.html, styles.css o uja.jpeg)
                             dos.write(("Content_Length:"+data.length+"\r\n").getBytes());
                             dos.write("\r\n".getBytes()); //Fin de cabeceras;
                             dos.flush();
@@ -111,7 +110,7 @@ public HttpConnection(Socket s) {
 
 	}
     protected byte[] readFile(String path) throws FileNotFoundException,IOException{
-        File f=new File("."+path);//donde metes la pagina es donde esta la carpeta de www y nsq mas de la practica
+        File f=new File("."+path);//Path será lo que pones a continuación de 192.168.1.10, en nuestro caso o /index.html o /styles.css o /uja.jpeg
         FileInputStream fis=new FileInputStream(f);
         byte []datos = new byte[(int)f.length()];
         fis.read(datos);
@@ -120,6 +119,7 @@ public HttpConnection(Socket s) {
         // return ("<html><body><h1>Hola "+path+"</h1></body></html>").getBytes();
     }
     protected String getContentType(String path){
+        // Como path es el index.html, styles.css o uja.jpeg, lee cuál de los 3 es en función de cómo termina.
         if(path.endsWith(".html") || path.endsWith(".htm")){
             return "text/html";
         }else if(path.endsWith(".jpeg") || path.endsWith(".jpg")){
